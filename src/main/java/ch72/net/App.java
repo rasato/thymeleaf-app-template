e81245d698d91520ae4f2d7d5905661282044c70
rasato <a.sato.uke@gmail.com>
2016-07-27 18:51:14 +0900

    generate archetype

---
+++ src/main/java/ch72/net/App.java
@@ -0,0 +1,13 @@
+package ch72.net;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
